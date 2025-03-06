19e2bc6d63765ca57ad5ccfb75f659aca5688e22
jessicarmiller <jessica.miller@hbi.com>
2025-03-06 22:09:52 +0000

    Added Blue/Green Prod delivery

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -17,13 +17,13 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <p>CI/CD Test</p>
+      <p>This is green!</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR test
+          Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
