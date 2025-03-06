f194e666e649312e8ca133c3962527af9f42c48e
jessicarmiller <jessica.miller@hbi.com>
2025-03-06 20:35:05 +0000

    Added Prod delivery

--- app-cdk/lib/pipeline-cdk-stack.ts
+++ app-cdk/lib/pipeline-cdk-stack.ts
@@ -12,6 +12,7 @@ import * as ecsPatterns from 'aws-cdk-lib/aws-ecs-patterns';
 interface ConsumerProps extends StackProps {
   ecrRepository: ecr.Repository,
   fargateServiceTest: ecsPatterns.ApplicationLoadBalancedFargateService,
+  fargateServiceProd: ecsPatterns.ApplicationLoadBalancedFargateService,
 }
 
 export class PipelineCdkStack extends Stack {
@@ -148,7 +149,23 @@ export class PipelineCdkStack extends Stack {
           service: props.fargateServiceTest.service,
           input: dockerBuildOutput,
         }),
-      ]
+      ],
+    });
+
+    pipeline.addStage({
+      stageName: 'Deploy-Production',
+      actions: [
+        new codepipeline_actions.ManualApprovalAction({
+          actionName: 'Approve-Deploy-Prod',
+          runOrder: 1,
+        }),
+        new codepipeline_actions.EcsDeployAction({
+          actionName: 'Deploy-Fargate-Prod',
+          service: props.fargateServiceProd.service,
+          input: dockerBuildOutput,
+          runOrder: 2,
+        }),
+      ],
     });
 
     new CfnOutput(this, 'SourceConnectionArn', {
